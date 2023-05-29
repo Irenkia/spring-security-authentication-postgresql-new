fa7dae6f86cd1b7b2f4eaa245d2cc3ff26a77ea6
Iryna Tiutiun <tiutiuniryna@gmail.com>
2023-05-29 21:20:53 +0200

    Initial project

---
+++ src/main/java/com/example/springsecurityauthenticationpostgresqlnew/SpringSecurityAuthenticationPostgresqlNewApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityauthenticationpostgresqlnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAuthenticationPostgresqlNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityAuthenticationPostgresqlNewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springsecurityauthenticationpostgresqlnew/SpringSecurityAuthenticationPostgresqlNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityauthenticationpostgresqlnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAuthenticationPostgresqlNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
